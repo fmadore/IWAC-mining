fe0e4142292591d11fa3224c3c6e8cad2d260df9
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2024-11-29 14:22:15 +0100

    Update config.js

--- Integrisme/Word_cloud/js/config.js
+++ Integrisme/Word_cloud/js/config.js
@@ -1,6 +1,6 @@
 // Configuration constants
-const CONFIG = {
-    dataUrl: 'https://raw.githubusercontent.com/fmadore/Mining_IWAC/main/Integrisme/Word_cloud/data/word_frequencies.json',
+window.CONFIG = {
+    dataUrl: 'https://github.com/fmadore/Mining_IWAC/raw/refs/heads/main/Integrisme/Word_cloud/data/word_frequencies.json',
     width: 800,
     height: 600,
     padding: 5,
@@ -8,14 +8,14 @@ const CONFIG = {
     maxFontSize: 60
 };
 
-const WORD_CATEGORIES = {
+window.WORD_CATEGORIES = {
     religious: ['religieux', 'musulman', 'islam', 'islamique', 'religion', 'imam', 'mosquée', 'prophète', 'coran', 'dieu', 'foi'],
     political: ['politique', 'président', 'ministre', 'gouvernement', 'état', 'national', 'parti'],
     social: ['dialogue', 'communauté', 'association', 'social', 'culture'],
     places: ['ivoire', 'burkina', 'pays', 'monde', 'faso', 'bénin']
 };
 
-const CATEGORY_COLORS = {
+window.CATEGORY_COLORS = {
     religious: d3.interpolateReds,
     political: d3.interpolateBlues,
     social: d3.interpolateGreens,
