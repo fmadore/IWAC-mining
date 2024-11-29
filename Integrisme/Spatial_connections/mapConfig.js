b3e9849154570f619ebce48cdd1b5d500712eb3d
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2024-11-29 09:03:19 +0100

    Create mapConfig.js

---
+++ Integrisme/Spatial_connections/mapConfig.js
@@ -0,0 +1,26 @@
+const MapConfig = {
+    width: 960,
+    height: 600,
+    projection: {
+        scale: 1000,
+        center: [-4, 12],  // Center on West Africa
+    },
+    colors: {
+        background: "#f0f0f0",
+        land: "#f0f0f0",
+        stroke: "#fff",
+        circles: "red"
+    },
+    zoom: {
+        min: 1,
+        max: 8
+    },
+    circle: {
+        minRadius: 5,
+        maxRadius: 25,
+        minOpacity: 0.5,
+        maxOpacity: 0.8
+    }
+};
+
+export default MapConfig; 
